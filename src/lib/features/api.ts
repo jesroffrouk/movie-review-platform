4f3ed5bbf2ba7d896af354ae4b8f3c21b4ff3d91
practice <jesroffrouk@gmail.com>
2025-06-04 14:55:00 +0530

    fixed base url for rtk query

--- src/lib/features/api.ts
+++ src/lib/features/api.ts
@@ -3,7 +3,7 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_BACKEND_URI!}),
+    baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
     tagTypes: ['Review', 'userDetails'],
     endpoints: (build) => ({
         getUser: build.query<Userdata , void>({
